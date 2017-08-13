c5c351d06c0d3fe33a3ca7c5e59e5d067788fea1
安藤春香 <>
2017-08-13 21:50:24 +0900

    gmail set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,18 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
   config.action_mailer.default_url_options = { :host => 'intense-waters-57765.herokuapp.com' }
+    config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:        "smtp.gmail.com",
+      port:           587,
+      authentication: "plain",
+      user_name:      "haruan2394@gmail.com",
+      password:       "Andou2394",
+      domain:         'heroku.com',
+      authentication: "login",
+      enable_starttls_auto: true
+  }
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
