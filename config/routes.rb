14a374459b9484b3f5910c99aa2ace02011fe4bf
安藤春香 <>
2017-08-15 20:26:06 +0900

    edit user_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {registrations: "users/registrations"}
+  devise_scope :user do
+ post 'users/sign_up/confirm' => 'users/registrations#confirm'
+ post 'users/sign_up/complete' => 'users/registrations#complete'
+end
    resources :users, only: [:show]
   resources :user_details, only: [:new,:create,:edit,:update]
 
